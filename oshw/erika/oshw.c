f1ead066dc0845d96ab02e73a626c20381ddc516
Claudio Scordino <claudio@evidence.eu.com>
2019-01-03 10:32:05 +0100

    OSHW implementation for ERIKA Enterprise

--- oshw/erika/oshw.c
+++ oshw/erika/oshw.c
@@ -6,7 +6,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <machine/endian.h>
 #include "oshw.h"
+
+#define MAX_ADAPTERS	3
+ec_adaptert	adapters [MAX_ADAPTERS];
 
 /**
  * Host to Network byte order (i.e. to big endian).
@@ -14,9 +18,10 @@
  * Note that Ethercat uses little endian byte order, except for the Ethernet
  * header which is big endian as usual.
  */
-uint16 oshw_htons(uint16 host)
+inline uint16 oshw_htons(uint16 host)
 {
-   return 1;
+	// __htons() is provided by the bare-metal x86 compiler
+	return __htons(host);
 }
 
 /**
@@ -25,24 +30,31 @@ uint16 oshw_htons(uint16 host)
  * Note that Ethercat uses little endian byte order, except for the Ethernet
  * header which is big endian as usual.
  */
-uint16 oshw_ntohs(uint16 network)
+inline uint16 oshw_ntohs(uint16 network)
 {
-   return 1;
+	// __ntohs() is provided by the bare-metal x86 compiler
+	return __ntohs(network);
 }
 
 /** Create list over available network adapters.
  * @return First element in linked list of adapters
  */
-ec_adaptert * oshw_find_adapters(void)
+ec_adaptert* oshw_find_adapters(void)
 {
-
-   return NULL;
+	// TODO: call eth_discover_devices()
+	// if (devs_nb < 1)
+	//	return NULL;
+	// Iterate over devs[i] and fill as follows:
+	//	sprintf(adapters[i].name, "%d\n", i); 
+	//	sprintf(adapters[i].desc, "%d\n", i); 
+	//	adapters[i].next = ...
+	return &(adapters[0]);
 }
 
 /** Free memory allocated memory used by adapter collection.
  * @param[in] adapter = First element in linked list of adapters
  * EC_NOFRAME.
  */
-void oshw_free_adapters(ec_adaptert * adapter)
+void oshw_free_adapters(ec_adaptert *adapter)
 {
 }
